e05fe503506ad64c0b5adf6711c425f8ccd46882
takashi145 <t.shuhei0203@gmail.com>
2023-01-14 20:22:42 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/saga-blue/theme.css"
+import "primevue/resources/primevue.min.css"
+import "primeicons/primeicons.css"
+import "primeflex/primeflex.css"
 
-createApp(App).use(router).mount('#app')
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+const pinia = createPinia();
+
+const app = createApp(App);
+
+app.use(PrimeVue).use(pinia).use(router);
+app.component('Button', Button);
+app.component('InputText', InputText);
+app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
+<script setup>
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <div class="m-auto mt-8 surface-card p-4 shadow-2 border-round w-full lg:w-6">
+        <div class="text-center mb-5">
+            <div class="text-900 text-3xl font-medium mb-3">Sign In</div>
+            <span class="text-600 font-medium line-height-3">アカウントをお持ちではありませんか？</span>
+            <router-link to="#" class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">新規登録</router-link>
+        </div>
+
+        <div>
+            <label for="email" class="block text-900 font-medium mb-2">Email</label>
+            <InputText id="email" type="text" placeholder="Email address" class="w-full mb-3" />
+
+            <label for="password" class="block text-900 font-medium mb-2">Password</label>
+            <InputText id="password" type="password" placeholder="Password" class="w-full mb-3" />
+
+            <Button label="Sign In" icon="pi pi-user" class="w-full"></Button>
+        </div>
+    </div> -->
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
