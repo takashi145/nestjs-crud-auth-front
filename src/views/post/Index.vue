86767b82da09d312a6ebbb3480ca3a5a9fbfd04e
takashi145 <t.shuhei0203@gmail.com>
2023-01-18 23:49:48 +0900

    delete post

--- src/views/post/Index.vue
+++ src/views/post/Index.vue
@@ -2,19 +2,41 @@
 import axios from "axios";
 import { ref, onMounted } from "vue";
 import Card from 'primevue/card';
+import Dialog from 'primevue/dialog';
 
 
 onMounted(async () => {
-  await getPost();
+  await getPostList();
 })
 
 const posts = ref([]);
 
-const getPost = async () => {
+const post_id = ref(null);
+
+const displayConfirmation = ref(false);
+
+const openConfirmation = id => {
+    displayConfirmation.value = true;
+    post_id.value = id;
+};
+
+const closeConfirmation = () => {
+    displayConfirmation.value = false;
+};
+
+const getPostList = async () => {
   await axios.get('/post')
   .then(res => {
     posts.value = res.data;
   })
+}
+
+const deletePost = async () => {
+  if(post_id.value) {
+    await axios.delete(`/post/${post_id.value}`)
+    await getPostList();
+  }
+  closeConfirmation();
 }
 </script>
 <template>
@@ -31,10 +53,19 @@ const getPost = async () => {
               <Button>編集</Button>
             </router-link>
 
-            <router-link to="/">
-              <Button class="p-button-danger">編集</Button>
-            </router-link>
+            <Button label="削除" class="p-button-danger" @click="openConfirmation(post.id)" />
         </template>
     </Card>
+
+    <Dialog header="Confirmation" v-model:visible="displayConfirmation" :breakpoints="{'960px': '75vw', '640px': '90vw'}" :style="{width: '350px'}" :modal="true">
+        <div class="confirmation-content">
+            <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+            <span>本当に削除してもよろしいですか?</span>
+        </div>
+        <template #footer>
+            <Button label="No" icon="pi pi-times" @click="closeConfirmation" class="p-button-text"/>
+            <Button label="Yes" icon="pi pi-check" @click="deletePost" class="p-button-text" autofocus />
+        </template>
+    </Dialog>
   </div>
 </template>
